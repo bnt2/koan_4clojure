cf953977b69dc7add995ce79882843e6d77fdbbf
Acestus <wme.weeks@gmail.com>
2021-06-14 07:15:02 -0500

    New App

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to koan_4clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/koan_4clojure/core.clj
@@ -0,0 +1,7 @@
+(ns koan-4clojure.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
