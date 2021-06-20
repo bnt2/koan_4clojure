834ca10906417cb416ec1a6e9413c97b96a8c626
Acestus <wme.weeks@gmail.com>
2021-06-19 21:10:51 -0500

    more answers

--- src/koan_4clojure/core.clj
+++ src/koan_4clojure/core.clj
@@ -46,3 +46,61 @@ Write a function which takes a variable number of parameters and returns the max
 (= (__ 45 67 11) 67)
 (reduce + [1 2 3 4 5])
 (reduce + 6 [2 3])
+({[]})
+(seq '(1))
+(seq [1 2])
+(seq "abc")
+(seq [])
+(seq )
+(if nil "logical true" "logical false")
+(if false "logical true" "logical true")
+(if 0 "logical true" "logical false")
+(if true "then branch")
+(defn max_number [x y] 
+  (if (x > y) x y))
+(fn [& more] 
+  (reduce
+   (fn 
+     [x y]
+     (if (> x y) x y)
+     )
+   more))                
+(fn [& coll]
+  (reduce #(if (> %2 %1) %2 %1) coll))
+; #57
+(= (5 4 3 2 1) ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5))
+
+((fn foo [x] (when (> x 0) (conj (foo (dec x)) x)))5)
+(= (__ (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (__))
+   5)
+(last (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
+(loop [x 5
+         result []]
+    (if (> x 0)
+      (recur (dec x) (conj result (+ 2 x)))
+      result))
+((fn [% more] (conj % more)) * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+(fn [f & maps]
+  (letfn [(step [m1 m2]
+            (if (seq m2)
+              (let [first-ele-in-m2 (first m2)
+                    v-in-m1 (m1 (key first-ele-in-m2))]
+                (if v-in-m1
+                  (step
+                    (assoc m1 (key first-ele-in-m2) (f v-in-m1 (val first-ele-in-m2)))
+                    (rest m2))
+                  (step
+                    (assoc m1 (key first-ele-in-m2) (val first-ele-in-m2))
+                    (rest m2))))
+              m1))]
+    (reduce step maps)))
+(fn [str] (sort-by #(.toLowerCase %) (re-seq #"\w+" str)))
+(fn [s]
+  (sort-by #(.toLowerCase %)
+    (re-seq #"[a-zA-Z]+" s)))
+(map inc (take 3 (drop 2 [2 5 4 1 3 6])))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce +))
+
+#((get :a {:a nil :b 2}))
